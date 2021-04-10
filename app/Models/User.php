31dca127a1380e122d47a7fe37f829329f5922cd
aliho <a.hodavand@yahoo.com>
2021-04-10 13:18:18 +0430

    add scope for creating user

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -46,5 +48,17 @@ class User extends Authenticatable
     public function avatar()
     {
         return $this->morphOne(Image::class, 'imageable');
+    }
+
+    // Local Scopes
+
+    public function scopeCreateUser($query, $request)
+    {
+        if ($request->hasFile('resume')) {
+            $request['resume_path'] = $request->file('resume')->store('resumes');
+        }
+        $request['password'] = Hash::make($request['password']);
+        $request['remember_token'] = Str::random(10);
+        return $this->create($request->all());
     }
 }
