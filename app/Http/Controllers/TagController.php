9cae75ae2dc49dbb870016d04fb8af42600e33c5
alihoda <a.hodavand@yahoo.com>
2021-06-16 13:30:19 +0430

    edit index and show funtions

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\ProductListTagResource;
 use App\Http\Resources\ProductTagResource;
 use App\Models\Tag;
 
@@ -11,11 +10,14 @@ class TagController extends Controller
 
     public function index()
     {
-        return ProductListTagResource::collection(Tag::has('products')->withCount('products')->get());
+        $tags = Tag::has('products')->withCount('products')->get();
+        return ProductTagResource::collection($tags);
     }
 
     public function show($tag)
     {
-        return new ProductTagResource(Tag::with(['products', 'products.user', 'products.tags'])->findOrFail($tag));
+        return new ProductTagResource(Tag::with(['products', 'products.user', 'products.tags'])
+            ->withCount('products')
+            ->findOrFail($tag));
     }
 }
